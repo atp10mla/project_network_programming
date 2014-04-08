525454577344c15f19801640dbc1f745c5f9e211
atp10mla <atp10mla@student.lu.se>
2014-04-08 21:20:47 +0200

    Add socket, os, is to client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,34 @@
 package client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {	
+	
 	public static void main(String[]args) {
+		Socket socket = null;
+		try {
+			socket = new Socket("localhost", 10000);
+		} catch (UnknownHostException e) {
+			System.out.println("Unknown host");
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println("Could not connect to server.");
+			System.exit(0);
+		}
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = socket.getInputStream();
+			os = socket.getOutputStream();
+		} catch (IOException e) {
+			System.out.println("Fail to open output/input stream.");
+			System.exit(0);
+		}
+		
 		
 	}
 
