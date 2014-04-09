e95db19401980b61aec90d477289882f0d7e4e6e
Daniel Forsman <danielforsman2@gmail.com>
2014-04-09 14:29:49 +0200

    Beginning of server.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,24 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 	public static void main(String[]args) {
-
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(5000);
+		} catch (IOException e) {
+			System.out.println("Could not set up server");
+			System.exit(1);
+		}
+		
+		try {
+			Socket socket = server.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
